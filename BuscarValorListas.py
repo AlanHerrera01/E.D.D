5a6ae27e6d8d17c6b4c83f75c1008ec369117d86
AlanHerrera01 <107574787+AlanHerrera01@users.noreply.github.com>
2023-05-15 08:36:26 -0500

    Update BuscarValorListas.py

--- BuscarValorListas.py
+++ BuscarValorListas.py
@@ -1,8 +1,11 @@
+#DECLARACIÓN DE FUNCIÓN DE BUSQUEDA
 def buscar_valor_en_lista(lista, valor):
     if valor in lista:
         return print("El valor ha sido encontrado")
     else:
         return print("El valor no ha sido encontrado")
+#CREACCIÓN DE LISTAS
 mi_lista = [1, 3, 5, 7, 9]
+#LLAMADO Y RESULTADO
 resultado = buscar_valor_en_lista(mi_lista, 3)
 print(resultado) 
